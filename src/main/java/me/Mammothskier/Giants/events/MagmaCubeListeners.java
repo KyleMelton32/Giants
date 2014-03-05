f8d365b17b911ad91f84478b1355262d232c8faf
Mammothskier <fs2dwyab@gmail.com>
2014-03-04 22:29:02 -0800

    Fixed slime and magma cube health event

--- src/main/java/me/Mammothskier/Giants/events/MagmaCubeListeners.java
+++ src/main/java/me/Mammothskier/Giants/events/MagmaCubeListeners.java
@@ -121,8 +121,8 @@ public class MagmaCubeListeners implements Listener {
 		} catch (Exception e) {
 			health = 100;
 		}
-		 MagmaCube magmacube = (MagmaCube)event.getEntity();
-		 s = magmacube.getSize();
+		 Entity magmacube = (MagmaCube)event.getEntity();
+		 s = ((MagmaCube) magmacube).getSize();
 		if((event.getEntityType() == EntityType.MAGMA_CUBE) && (s ==  size)){
 			event.getEntity().setMaxHealth(health);
 		}
