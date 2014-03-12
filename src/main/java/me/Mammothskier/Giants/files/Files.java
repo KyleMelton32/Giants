b5f00762957754ec12be0283d88540e3be3059c0
Mammothskier <fs2dwyab@gmail.com>
2014-03-11 18:36:08 -0700

    changed file names for biome files

--- src/main/java/me/Mammothskier/Giants/files/Files.java
+++ src/main/java/me/Mammothskier/Giants/files/Files.java
@@ -2,9 +2,9 @@ package me.Mammothskier.Giants.files;
 
 public enum Files {
 	CONFIG("plugins/Giants/config.yml"),
-	GIANT("plugins/Giants/Giant/giant.yml"), GIANTBIOMES("plugins/Giants/Giant/biomes.yml"),
-	MAGMACUBE("plugins/Giants/Magma Cube/magmacube.yml"), MAGMACUBEBIOMES("plugins/Giants/Magma Cube/biomes.yml"),
-	SLIME("plugins/Giants/Slime/slime.yml"), SLIMEBIOMES("plugins/Giants/Slime/biomes.yml");
+	GIANT("plugins/Giants/Giant/giant.yml"), GIANTBIOMES("plugins/Giants/Giant/giant biomes.yml"),
+	MAGMACUBE("plugins/Giants/Magma Cube/magmacube.yml"), MAGMACUBEBIOMES("plugins/Giants/Magma Cube/magma cube biomes.yml"),
+	SLIME("plugins/Giants/Slime/slime.yml"), SLIMEBIOMES("plugins/Giants/Slime/slime biomes.yml");
 	
 	private final String _path;
 
