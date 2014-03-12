2d5281c6f7743aebb08624ea1c28e50ff88ed035
Mammothskier <fs2dwyab@gmail.com>
2014-03-11 20:03:14 -0700

    Fixed onEnable message

--- src/main/java/me/Mammothskier/Giants/Giants.java
+++ src/main/java/me/Mammothskier/Giants/Giants.java
@@ -7,6 +7,7 @@ import me.Mammothskier.Giants.utils.API;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.Metrics;
 
@@ -17,7 +18,9 @@ public class Giants extends JavaPlugin{
 	@Override
 	public void onEnable(){
 		new API(this);
-		Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "[Giants]" + ChatColor.GREEN + Bukkit.getName() + " Version " + Bukkit.getVersion() + " Has Been Enabled!");
+		PluginDescriptionFile pdf = this.getDescription();
+		
+		Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "[Giants]" + ChatColor.GREEN + pdf.getName() + " Version " + pdf.getVersion() + " Has Been Enabled!");
 		try {
 		    Metrics metrics = new Metrics(this);
 		    metrics.start();
