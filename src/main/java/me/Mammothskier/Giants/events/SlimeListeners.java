011dd79c2981646e979a5a62fa0068389812aad1
Mammothskier <fs2dwyab@gmail.com>
2014-04-27 16:45:49 -0700

    Fixed unused imports

--- src/main/java/me/Mammothskier/Giants/events/SlimeListeners.java
+++ src/main/java/me/Mammothskier/Giants/events/SlimeListeners.java
@@ -11,7 +11,6 @@ import me.Mammothskier.Giants.utils.API;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
