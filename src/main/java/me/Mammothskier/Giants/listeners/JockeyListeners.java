8c340cfbe0eaaa690dd24e956605ef597d4c2930
Mammothskier <fs2dwyab@gmail.com>
2014-11-06 01:45:55 -0800

    Removing unneeded event listener for jockeys

--- src/main/java/me/Mammothskier/Giants/listeners/JockeyListeners.java
+++ src/main/java/me/Mammothskier/Giants/listeners/JockeyListeners.java
@@ -1,16 +1,7 @@
 package me.Mammothskier.Giants.listeners;
 
 import me.Mammothskier.Giants.Giants;
-import me.Mammothskier.Giants.utils.API;
-
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Giant;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Slime;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreatureSpawnEvent;
 
 public class JockeyListeners implements Listener {
 	private Giants _jockeys;
@@ -19,20 +10,5 @@ public class JockeyListeners implements Listener {
 	public JockeyListeners(Giants jockey) {
 		_jockeys = jockey;
 		_jockeys.getServer().getPluginManager().registerEvents(this, jockey);
-	}
-	
-	@EventHandler
-	public void JockeySpawnEvent (CreatureSpawnEvent event) {
-		Entity entity = event.getEntity();
-
-		if (API.isGiantMagmaCube(entity) || API.isGiantSlime(entity)) {
-			Slime cube = (Slime) entity;
-			for (Entity entity2 : cube.getNearbyEntities(15, 12, 15)) {
-				if (API.isGiant(entity2)) {
-					Giant giant = (Giant) entity2;
-					cube.setPassenger(giant);
-				}
-			}
-		}
 	}
 }
