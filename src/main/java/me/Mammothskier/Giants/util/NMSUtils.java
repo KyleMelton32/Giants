3eef44c64459f7d7ce5e75623283dab19922e42b
Kyle Melton <fs2dwyab@gmail.com>
2015-06-14 16:05:22 -0700

    Revert "Setting up maven modules"
    This reverts commit 72455fd97df5e86a528f656ec4adaeb997d96405.

---
+++ src/main/java/me/Mammothskier/Giants/util/NMSUtils.java
@@ -0,0 +1,28 @@
+package me.Mammothskier.Giants.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bukkit.Bukkit;
+
+public class NMSUtils {
+	
+	public static String getBukkitVersion() {
+		Matcher matcher = Pattern.compile("v\\d+_\\d+_R\\d+").matcher(Bukkit.getServer().getClass().getPackage().getName());
+		if (matcher.find()) {
+			return matcher.group();
+		} else {
+			return null;
+		}
+	}
+	
+	public static String getMinecraftVersion() {
+		Matcher matcher = Pattern.compile("(\\(MC: )([\\d\\.]+)(\\))").matcher(Bukkit.getVersion());
+		if (matcher.find()) {
+			return matcher.group(2);
+		} else {
+			return null;
+		}
+	}
+
+}
